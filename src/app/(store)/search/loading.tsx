af9201c36617767668871b408b342da9f3d9882b
Bruno Fukumori <brunofukumori@gmail.com>
2023-12-19 01:57:03 -0300

    feat: loading for search and fix product link

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,3 +1,26 @@
+'use client';
+
+import { Skeleton } from '@/components/skeleton';
+import { useSearchParams } from 'next/navigation';
+
 export default function SearchLoading() {
-  return <p>Carregando busca...</p>;
+  const searchParams = useSearchParams();
+  const query = searchParams.get('q');
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultados para: {query}
+        <span className="font-semibold"></span>
+      </p>
+      <div className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-[340px]" />
+        <Skeleton className="h-[340px]" />
+        <Skeleton className="h-[340px]" />
+        <Skeleton className="h-[340px]" />
+        <Skeleton className="h-[340px]" />
+        <Skeleton className="h-[340px]" />
+      </div>
+    </div>
+  );
 }
--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -38,7 +38,7 @@ export default async function Search({ searchParams }: SearchProps) {
         {products.map((product) => (
           <Link
             key={product.id}
-            href={product.slug}
+            href={`/product/${product.slug}`}
             className="group rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end relative"
           >
             <Image
