b521239447dff1d9001ae67b9698375cf86b2585
Bruno Fukumori <brunofukumori@gmail.com>
2023-12-19 03:00:58 -0300

    refactor: custom commands

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -3,25 +3,22 @@ describe('add product to cart', () => {
     cy.visit('/');
   });
   it('should be able to navigate to the product page and add it to the cart', () => {
-    cy.get('a[href^="/product"]').first().click();
-    cy.location('pathname').should('include', '/product');
-    cy.contains('Adicionar ao carrinho').click();
+    cy.navigateToProductPage();
+    cy.addToCart();
     cy.contains('Cart (1)').should('exist');
   });
 
   it('should not count duplicated products in cart', () => {
-    cy.get('a[href^="/product"]').first().click();
-    cy.location('pathname').should('include', '/product');
-    cy.contains('Adicionar ao carrinho').click();
-    cy.contains('Adicionar ao carrinho').click();
+    cy.navigateToProductPage();
+    cy.addToCart();
+    cy.addToCart();
     cy.contains('Cart (1)').should('exist');
   });
 
   it('should be able to search for a product and add it to the cart', () => {
     cy.get('input[name=q]').type('moletom').parent('form').submit();
-    cy.get('a[href^="/product"]').first().click();
-    cy.location('pathname').should('include', '/product');
-    cy.contains('Adicionar ao carrinho').click();
+    cy.navigateToProductPage();
+    cy.addToCart();
     cy.contains('Cart (1)').should('exist');
   });
 });
